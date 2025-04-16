e6fe5c8dd49effc4b9ce24c66204c14efcba0f33
Brandon Harris <brandon@relentnet.com>
2025-04-16 17:22:41 -0500

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h1 className="font-bold text-2xl underline text-red-700">Hello react</h1>);
